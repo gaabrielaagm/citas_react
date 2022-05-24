9e3a50597b5e51c27384830f516ea633ae20ca5b
Gabriela GM <gaabrielaagm@github.com>
2022-05-23 21:17:59 -0500

    First and last commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react"
+import Formulario from "./components/Formulario"
+import Header from "./components/Header"
+import ListadoPacientes from "./components/ListadoPacientes"
+
+function App() {
+  const [pacientes, setPacientes] = useState([{nombre:'a', propietario:'a', email:'a@a.com', id:'gnsfsd9f3'}]);
+  const [paciente, setPaciente] = useState({});
+
+  const eliminarPaciente = (id) => {
+    const pacientesActualizados = pacientes.filter(paciente => paciente.id !== id);
+    setPacientes(pacientesActualizados);
+  }  
+
+  const obtenerLocalStorage = () => {
+    const pacientesLS = (localStorage.getItem('pacientes')) ? JSON.parse(localStorage.getItem('pacientes')) : [];
+    setPacientes(pacientesLS);
+  }
+
+  useEffect(() => {
+    obtenerLocalStorage();
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('pacientes', JSON.stringify(pacientes));
+  }, [pacientes])
+
+  return (
+    <div className="container mx-auto mt-20">
+      <Header />
+      <div className="mt-12 md:flex w-full">
+        <Formulario
+          pacientes={pacientes}
+          setPacientes={setPacientes}
+          paciente={paciente}
+          setPaciente={setPaciente}
+        />
+        <ListadoPacientes
+          pacientes={pacientes} 
+          setPaciente={setPaciente}
+          eliminarPaciente={eliminarPaciente}
+        />
+      </div>
+    </div>
+  ) 
+}
+
+export default App
